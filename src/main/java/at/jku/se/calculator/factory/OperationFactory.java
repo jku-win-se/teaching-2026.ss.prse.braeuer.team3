c0eea65735211d0652e31fcba87dc1e86e788c59
Oliver Sugic <osugic@gmail.com>
2026-03-13 14:39:18 +0100

    #2 [0.5h] added multiplication

--- src/main/java/at/jku/se/calculator/factory/OperationFactory.java
+++ src/main/java/at/jku/se/calculator/factory/OperationFactory.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.Logger;
 import at.jku.se.calculator.CalcAction;
 import at.jku.se.calculator.operators.AddOperation;
 import at.jku.se.calculator.operators.NullOperation;
+import at.jku.se.calculator.operators.MultiplyOperation;
 
 /**
  * Factory class for {@link ICalculationOperation}.
@@ -37,7 +38,7 @@ public class OperationFactory {
 		case DIV:
 			return new NullOperation();
 		case MULT:
-			return new NullOperation();
+			return new MultiplyOperation();
 		case SUB:
 			return new NullOperation();
 		default:
