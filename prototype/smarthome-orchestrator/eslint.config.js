af2e7e8def5b1112498f587540cb6f509b8ccf08
SimonFalkner02 <sfalkner82@gmail.com>
2026-03-19 21:09:13 +0100

    #7 [1.0h] remove java project, setup pipeline to build prototype

---
+++ prototype/smarthome-orchestrator/eslint.config.js
@@ -0,0 +1,40 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("@angular-eslint/eslint-plugin");
+const angularTemplate = require("@angular-eslint/eslint-plugin-template");
+const angularTemplateParser = require("@angular-eslint/template-parser");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+    ],
+    plugins: {
+      "@angular-eslint": angular,
+    },
+    rules: {
+      ...angular.configs.recommended.rules,
+    },
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+  {
+    files: ["**/*.html"],
+    plugins: {
+      "@angular-eslint/template": angularTemplate,
+    },
+    languageOptions: {
+      parser: angularTemplateParser,
+    },
+    rules: {
+      ...angularTemplate.configs.recommended.rules,
+    },
+  }
+);
